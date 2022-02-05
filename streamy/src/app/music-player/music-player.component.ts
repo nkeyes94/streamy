6e8e2b73876543e7b9d7fde00e3ca3b93eb4a583
Nate <nkeyes@angusenergy.com>
2022-02-05 10:31:14 -0500

    Added working music player with service

--- streamy/src/app/music-player/music-player.component.ts
+++ streamy/src/app/music-player/music-player.component.ts
@@ -1,31 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AudioService } from '../services/audio.service';
+import { CloudService } from '../services/cloud.service';
+import { StreamState } from '../interfaces/stream-state';
 
 @Component({
   selector: 'app-music-player',
   templateUrl: './music-player.component.html',
-  styleUrls: ['./music-player.component.css']
+  styleUrls: ['./music-player.component.scss']
 })
 export class MusicPlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public audioService: AudioService,
+    public cloudService: CloudService
+  ) { 
+    // * Get media files
+    cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+    // * Listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
+  }
+
+  playStream(url: any) { 
+    this.audioService.playStream(url).subscribe((events: any) => {
+      // Listen to events
+    });
+  }
+
+  openFile(file: any, index: number) { 
+    this.currentFile = { file, index};
+    this.audioService.stop();
+    this.playStream(file.url);
+  }
+  
+  // * Adding audio methods from our service to our component
+  // ? Pause
+  pause() { 
+    this.audioService.pause();
+  }
+  // ? Play
+  play() {
+    this.audioService.play();
+  }
+  // ? Stop
+  stop() {
+    this.audioService.stop();
+  }
+  // ? Next
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+  // ? Previous
+  previous() {
+    const index = this.currentFile -1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+  // ? Is first playing
+  isFirstPlaying() { 
+    return this.currentFile.index === 0;
+  }
+  // ? Is last playing
+  isLastPlaying() {
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  onSliderChangeEnd(change: { value: any; }) {
+    this.audioService.seekTo(change.value);
+  }
 
   ngOnInit(): void {
   }
+
+  files: Array<any> = [];
+  state: StreamState | undefined;
+  currentFile: any = {};
 
   songs: Array<any> = [
     {title: 'Test', artist: 'Dad'},
     {title: 'Test2', artist: 'Dad2'},
   ];
-  state = {};
   currentSong: any = {};
 
-  play(){}
-  pause(){}
-  isFirstPlaying(){
-    return false;
-  }
-  isLastPlaying() {
+  isPlaying() {
     return true;
   }
 
---
+++ streamy/src/app/services/cloud.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CloudService {
+  files: any = [
+    {
+      // tslint:disable-next-line: max-line-length
+      url:
+        "https://ia801609.us.archive.org/16/items/nusratcollection_20170414_0953/Man%20Atkiya%20Beparwah%20De%20Naal%20Nusrat%20Fateh%20Ali%20Khan.mp3",
+      name: "Man Atkeya Beparwah",
+      artist: "Nusrat Fateh Ali Khan"
+    }
+  ];
+
+  getFiles() {
+    return of(this.files);
+  }
+}
